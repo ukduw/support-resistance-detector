9de118f834d7c8582686cb120f54a91d48b2d281
ukduw <edhkmayo@gmail.com>
2025-08-29 08:31:47 -0400

    added dollar value input, default, return symbol list + dollar value float, fixed input strip

--- symbol_CLI.py
+++ symbol_CLI.py
@@ -1,13 +1,15 @@
 
 def symbol_cli():
+    default = 4000
     print("Input watchlist")
 
     while True:
         symbol_list = input(f"Enter symbol list, separated by commas: ").upper().split(", ")
         print(f"({len(symbol_list)}) {symbol_list}")
+        dollar_value = input(f"Position dollar value [{default}]: ")
 
         while True:
-            cont = input("Is this correct? (y/n): ").stip().lower()
+            cont = input("Is this correct? (y/n): ").strip().lower()
             if cont == "n":
                 break
             elif cont == "y":
@@ -18,6 +20,6 @@ def symbol_cli():
         if cont == "y":
             break
     
-    return symbol_list
+    return {"symbol_list": symbol_list, "dollar_value": float(dollar_value or default)}
 
 
