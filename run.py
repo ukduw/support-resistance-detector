23c12b14f68770a2e1f9a6d044ad61c2d8d82aa3
ukduw <edhkmayo@gmail.com>
2025-08-29 20:06:01 -0400

    added time condition to run rest of script (19:45 EDT, aftermarket close 20:00) - each module prints, so no need to add any in the if/else statement - also added module imports

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
+import pytz, datetime, time
+
 from symbol_CLI import symbol_cli
 from parameter_writer import parameter_writer
+from support_resistance_detector import support_resistance_detector
+
+eastern = pytz.timezone("US/Eastern")
+now = datetime.datetime.now(eastern)
+aftermarket_end_incoming = now.replace(hour=19, minute=45, second=0, microsecond=0) # 19:45 EDT / 00:45 BST
 
 # wrapper
 
@@ -10,5 +17,14 @@ from parameter_writer import parameter_writer
 
 if __name__ == "main":
     symbols = symbol_cli()
-    levels_dict = support_resistance_detector(symbols)
-    parameter_writer(levels_dict)
+
+    while True:
+        now = datetime.datetime.now(eastern)
+
+        if now >= aftermarket_end_incoming:
+            levels_dict = support_resistance_detector(symbols)
+            parameter_writer(levels_dict)
+        else: 
+            time.sleep(300) # 5min
+
+
