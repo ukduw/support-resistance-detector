4083d8f8d153fcfee8213f6c7b916f9db0af7461
ukduw <edhkmayo@gmail.com>
2025-08-31 13:55:43 -0400

    updated import for class

--- run.py
+++ run.py
@@ -2,11 +2,13 @@ import pytz, datetime, time
 
 from symbol_CLI import symbol_cli
 from parameter_writer import parameter_writer
-from support_resistance_detector import support_resistance_detector
+from support_resistance_detector import LevelDetector
 
 eastern = pytz.timezone("US/Eastern")
 now = datetime.datetime.now(eastern)
 aftermarket_end_incoming = now.replace(hour=19, minute=45, second=0, microsecond=0) # 19:45 EDT / 00:45 BST
+
+ld = LevelDetector()
 
 
 if __name__ == "main":
@@ -16,7 +18,10 @@ if __name__ == "main":
         now = datetime.datetime.now(eastern)
 
         if now >= aftermarket_end_incoming:
-            levels_dict = support_resistance_detector(symbols)
+            ld.fetch_candlestick_data(symbols)
+            ld.find_local_extrema()
+            levels_dict = ld.closest_extrema()
+            
             parameter_writer(levels_dict)
         else: 
             time.sleep(300) # 5min
