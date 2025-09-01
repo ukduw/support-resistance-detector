524dafd41f4f112386df0863b2b00f2dd1b6db0b
ukduw <edhkmayo@gmail.com>
2025-09-01 13:25:44 -0400

    fixed it for async level_detector

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import pytz, datetime, time
+import asyncio
 
 from symbol_CLI import symbol_cli
 from parameter_writer import parameter_writer
@@ -8,6 +9,9 @@ eastern = pytz.timezone("US/Eastern")
 now = datetime.datetime.now(eastern)
 aftermarket_end_incoming = now.replace(hour=19, minute=45, second=0, microsecond=0) # 19:45 EDT / 00:45 BST
 
+async def main():
+    await level_detector(symbols)
+
 
 if __name__ == "main":
     symbols = symbol_cli()
@@ -16,7 +20,7 @@ if __name__ == "main":
         now = datetime.datetime.now(eastern)
 
         if now >= aftermarket_end_incoming:
-            levels_dict = level_detector(symbols)
+            levels_dict = asyncio.run(main(symbols))
             parameter_writer(levels_dict)
         else: 
             time.sleep(300) # 5min
