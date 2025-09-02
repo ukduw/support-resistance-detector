c7d239f3252b9bf577d0d7b6439e1ed2f1658050
ukduw <edhkmayo@gmail.com>
2025-09-02 11:36:23 -0400

    added plans/some pseudocode for cli rewrite - needs multiple inputs/dictionary keys for lookback days so less unnecessarily large api requests

--- symbol_CLI.py
+++ symbol_CLI.py
@@ -23,3 +23,12 @@ def symbol_cli():
     return {"symbol_list": symbol_list, "dollar_value": float(dollar_value or default)}
 
 
+# re-write to prompt user to input 6 different lists
+    # fetch 5, 4, 3, 2, 1, and 0 days back
+        # 0 days means intraday only
+        # all other ones means intraday + x days
+            # use this to calculate lookback minutes in support_resistance_detector
+            # e.g. 2 days = 11:45 (intra) + 16hr (full day) * 2 (day count) = 43.75hrs
+    
+    # account for blank entry on certain day
+        # e.g. if len(symbol_list) == 0...
