92fa4cf0468ac82a4861ffb85d86ea068f9a790d
ukduw <edhkmayo@gmail.com>
2025-09-04 06:41:16 -0400

    added note on multi-index return, edited and moved plans/pseudocode

--- support_resistance_detector.py
+++ support_resistance_detector.py
@@ -9,46 +9,14 @@ import numpy as np
 from dotenv import load_dotenv
 import os
 
-
-# determine support/resistance levels - probably need to increase sensitivity for granular/weak levels
-# levels should have x% bounds - i.e. candles shouldn't have to bounce off a price perfectly to contribute to a level's count
-    # if so, using the upper and lower bound of 2 levels for entry/exit will increase risk %
-    # level * bound% is the variable that needs to be evaluated with the below logic
-# or, use average true range or standard deviation on top of these levels
-# not just single price point, but a small range
-    # again, remember to account for upper and lower bound for risk %
-# after determining levels, sort, iterate through, removing those that are within another's stdev
-    # these represent the same level
-    # which one to get rid of? the one that's from being touched less recently?
-
-# gist:
-    # pivot points (most common method):
-        # count pivot candles at price points (+ bound%)
-        # can score levels by number of pivot candles
-        # pivot candle = higher/lower than previous/neighboring candles
-    # local maxima/minima:
-        # local extrema over a sliding window of candlesticks
-        # in other words, price points where price previously reversed
-# remove noise via fractals?
-    # essentially candle pattern: (green vs red) 1. ggg-rr, 2. rrr-gg
-    # 1. reversal on hitting resistance, 2. reversal on hitting support
-
-
-# need logic to determine:
-    # levels closest to 5% of current price, within 10%
-        # what if equidistant to 5%? e.g. 3 vs 7, 1 vs 9
-    # just realized lower level should be 5-10% lower than the upper level used, not the intraday
-    # if excessively close to current price, default to next closest level
-    # if no upper in 5-10% range, default to <5 or >10?
-    # if no lower in 5-10% range, just default to intraday??
-# round to 4 significant figures
-
-# build dict of entry/exit parameters per symbol
-# return dict (to use in parameter_writer)
-
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 SECRET_KEY = os.getenv("SECRET_KEY")
+
+eastern = pytz.timezone("US/Eastern")
+
+historical_client = StockHistoricalDataClient(api_key=API_KEY, secret_key=SECRET_KEY)
+
 
 bar_data = {}
 intraday_prices = {}
@@ -59,11 +27,6 @@ local_extrema_sd = {}
 
 levels = {}
     # level strength score could be used for: 1. take-profit logic, 2. risk-off, 3. planning bigger plays
-
-
-eastern = pytz.timezone("US/Eastern")
-
-historical_client = StockHistoricalDataClient(api_key=API_KEY, secret_key=SECRET_KEY)
 
 
 # symbols:      (example: symbol_list3 skipped; empty day)
@@ -89,7 +52,7 @@ async def level_detector(symbols):
             start_time = now - datetime.timedelta(minutes=lookback_minutes)
 
             request_params = StockBarRequest(
-                symbol_or_symbols=symbols[key], 
+                symbol_or_symbols=symbols[key],     # this ALWAYS returns multi-index df, even if list only contains 1 symbol
                 timeframe=TimeFrame(5, TimeFrame.Minute),
                 start=start_time,
                 end=now,
@@ -123,17 +86,43 @@ async def level_detector(symbols):
 
 
     # calculate stdev per symbol
+    # levels should have stdev bounds - i.e. candles shouldn't have to bounce off a price perfectly to contribute to a level's count
+    # if so, using the upper and lower bound of 2 levels for entry/exit will increase risk %
+        # remember to account for upper and lower bound for risk %
+    # level +- stdev is the variable that needs to be evaluated with the below logic
+        # not just single price point, but a small range
+        # after determining levels, sort, iterate through, removing those that are within another's stdev
+    # these represent the same level
+    # which one to get rid of? the one that's been touched less recently? or less total touches?
+        # add removed level's score to the one that replaces it
+
 
     # price zones using below +- 1 stdev (daily, per bar...?)
     # take bar_data df, append local_extrema_sd with?:
         # 1. local extrema of sliding window
+            # local extrema over a sliding window of candlesticks
+            # in other words, price points where price previously reversed
         # 2. pivot points with strength score?
+            # count pivot candles at price points (+ bound%)
+            # can score levels by number of pivot candles
+            # pivot candle = higher/lower than previous/neighboring candles
         # 3. reduce noise via fractals?
-        # 4. calculate stdev
+            # essentially candle pattern: (green vs red) 1. ggg-rr, 2. rrr-gg
+            # 1. reversal on hitting resistance, 2. reversal on hitting support
 
-    # logic to find closest appropriate levels per symbol
-        # append levels with upper level + stdev, lower level - stdev, rounded to 4sf
-        # ...with the below format
+
+    # need logic to determine:
+        # levels closest to 5% of current price, within 10%
+            # what if equidistant to 5%? e.g. 3 vs 7, 1 vs 9
+        # just realized lower level should be 5-10% lower than the upper level used, not the intraday
+        # if excessively close to current price, default to next closest level
+        # if no upper in 5-10% range, default to <5 or >10?
+        # if no lower in 5-10% range, just default to intraday??
+    # round to 4 significant figures
+
+
+    # build dict of entry/exit parameters per symbol, with below format
+
 
     levels["dollar_value"] = dollar_value
     
